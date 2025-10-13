0bbd75ba5d062050c5d48379d9daad79a57dca0b
Rafis Shigapov <rafis1work@gmail.com>
2025-10-13 18:07:37 +0300

    add routing and NewNote page

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { NewNote } from './NewNote';
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
+function App() {
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Главная страница</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Показать</h1>} />
+          <Route path="edit" element={<h1>Редактировать</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
